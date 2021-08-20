08bb7d091db55a16d6a88a61c1daa7bb9a70e698
sandygoreraza <gorerazasandy10@gmail.com>
2021-08-20 23:47:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+	 
+
+
+    data() {
+        return {
+
+            
+            cv_nameform : 'Sandy Goreraza Resume',
+            
+            designation : 'Senior Full Stack Web Developer',
+            show_contactForm : true,
+            certificates_header : 'Professional Certifications'
+        }
+    }
+	
+	
+	
+})
+
