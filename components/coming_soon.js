a004326f73b7c3941a5d95fefa85ec0eda291f3a
sandygoreraza <gorerazasandy10@gmail.com>
2025-01-08 14:11:10 +0200

    Update coming_soon.js

--- components/coming_soon.js
+++ components/coming_soon.js
@@ -1,1 +1,24 @@
-
+app.component('coming-soon-contact-sandy-goreraza', {
+		
+	data(){
+		return{
+		coming_soon_text : ''
+          
+           
+		}
+		
+	},
+	
+	
+    template:
+          `
+ 
+ 
+ <!-- section coming soon -->
+		<section id="coming-soon" class="shadow-blue white-bg">
+	 
+		</section>
+ 
+ 
+`
+})
