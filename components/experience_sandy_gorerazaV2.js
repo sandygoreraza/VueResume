6dd5417e454bd42b26d6dd2226ca5acea5c595e8
sandygoreraza <gorerazasandy10@gmail.com>
2025-01-08 11:10:55 +0200

    Update experience_sandy_gorerazaV2.js

--- components/experience_sandy_gorerazaV2.js
+++ components/experience_sandy_gorerazaV2.js
@@ -22,7 +22,7 @@ app.component('pro-experience-sandy-goreraza', {
 			<div class="timeline">
 				<div class="entry">
 					<div class="title">
-                                                <span><img src="cinqueterre.jpg" class="rounded" alt="Cinque Terre" width="304" height="236"> </span>
+                                                <span><img src="cinqueterre.jpg" class="rounded" alt="Cinque Terre" width="152" height="118"> </span>
 						<span>March 2014 - Date </span>
 					</div>
 					<div class="body">
